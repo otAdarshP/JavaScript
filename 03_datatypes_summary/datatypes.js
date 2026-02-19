79c937e6efd23102ee12c5b44ae84207a19428fb
Adarsh Priyadarshi <adarshp0808@gmail.com>
2026-02-19 12:46:40 +0530

    Create data types

---
+++ 03_datatypes_summary/datatypes.js
@@ -0,0 +1,22 @@
+// Primitive (Call by Value): String, Number, Boolean, Undefined, Symbol, BigInt, Null
+// Reference [Non-Primitive (Call by Reference)]: Arrays, Objects, Functions 
+
+// Arrays:
+let myArr = ["Sanjay", "Dhritrashtra", "Pandu"]
+// Objects:
+let myObj = {
+    name: "Adarsh",
+    age: 22
+}
+// Functions:
+const myFun = function(){
+    console.log("Hello, world!")
+}
+
+// datatype of different VAL:
+// Undefined: "undefined"
+// Null: "Null"
+// Boolean: "Boolean"
+// Number: "Number"
+// String: "String"
+// Object: "Object", "Function"
